95965fea38de4709d160115c395a9ebaec0cd33c
lkinga7 <lk1ngaa7@gmail.com>
2015-03-23 10:52:41 +0800

    /*   数组 A 中任意两个相邻元素大小相差 1，现给定这样的数组 A 和目标整数 t， 找出 t 在数组 A 中的位置 */

--- baidu_1/main.c
+++ baidu_1/main.c
@@ -5,7 +5,16 @@
   数组 A 中任意两个相邻元素大小相差 1，现给定这样的数组 A 和目标整数 t，
 找出 t 在数组 A 中的位置
 */
-
+int findInArray(int array[],int target,int length){
+   int index = array[0] - target;
+   index =index >0?index:-index;
+   if(index > length)
+     return -1;
+   if(array[index] == target)
+    return index;
+   else
+    return -1;
+}
 int binary_search(int array[],int n,int value)
 {
     int left=0;
@@ -29,24 +38,9 @@ int binary_search(int array[],int n,int value)
     return -1;
 }
 
-int binarySearch(int *a ,int t,int length){
-  int middle;
-  middle = (length-1) >> 1;
-
-  while(middle != 0 && middle != length){
-      printf("i:%d\n",middle);
-      if(a[middle] < t)
-        middle += middle >> 1;
-      else if(a[middle] > t)
-        middle = middle >> 1;
-      else
-        return middle;
-   }
-  return -1;
-}
 int main()
 {
-   /* int *a = (int *)malloc(sizeof(int)*MAX);
+    int *a = (int *)malloc(sizeof(int)*MAX);
     int t,i;
     for(i = 0;i<MAX;i++){
             a[i] = i;
@@ -55,10 +49,7 @@ int main()
     t = rand();
 
     printf("t : %d\nindex:%d\n",t,binary_search(a,MAX,t));
-    //printf("t : %d\nindex:%d",t,binarySearch(a,t,MAX));
+    printf("t : %d\nindex:%d\n",t,findInArray(a,t,MAX));
     free(a);
-    */
-    int i= -1;
-    printf("%d",i>>10);
     return 0;
 }
